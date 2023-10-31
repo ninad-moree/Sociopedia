5c61893952e6f204685e840e145f8ccc5224ab34
Ninad More <ninadyogeshmore@gmail.com>
2023-10-31 19:40:58 +0530

    configuration and setup done

--- server/index.js
+++ server/index.js
@@ -7,3 +7,41 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
+import { error } from "console";
+
+// ####### CONFIGURATION ####### //
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+//  ####### FILE STORAGE ####### // 
+// -- when someone uploads images on your website, those images are stored using this multer at the location -> public/assets
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage});
+
+// ####### MONGOOSE SET UP ####### //
+const PORT = process.env.PORT ||  6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`Error: ${error}`));
