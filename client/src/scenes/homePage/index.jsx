833c550381e57015873c46300fd136b6cb72f18b
Ninad More <ninadyogeshmore@gmail.com>
2023-11-08 18:55:06 +0530

    user widget created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,32 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
+    const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
     return (
         <Box>
             <Navbar />
-      </Box>
+
+            <Box width="100%" padding="2rem 6%" display={isNonMobileScreen ? "flex" : "block"} gap="0.5rem" justifyContent="space-between">
+                <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath}/>
+                </Box>
+
+                <Box flexBasis={isNonMobileScreen ? "42%" : undefined} mt={isNonMobileScreen ? undefined : "2rem"}>
+
+                </Box>
+
+                {isNonMobileScreen && (
+                    <Box flexBasis="26%">
+
+                    </Box>
+                )}
+            </Box>
+        </Box>
     )
 }
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verfiyToken = async (req, res, next) => {
     try{
-        let token = req.header("Authorizaion");
+        let token = req.header("Authorization");
 
         if(!token) 
             return res.status(403).send("Access Denied");
