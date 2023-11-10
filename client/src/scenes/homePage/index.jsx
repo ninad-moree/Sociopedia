3d52969a585f57e43faaf7dba5ee8a4e82688870
Ninad More <ninadyogeshmore@gmail.com>
2023-11-10 19:20:08 +0530

    posts widgets done

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
     const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
@@ -19,6 +20,7 @@ const HomePage = () => {
 
                 <Box flexBasis={isNonMobileScreen ? "42%" : undefined} mt={isNonMobileScreen ? undefined : "2rem"}>
                     <MyPostWidget picturePath={picturePath} />
+                    <PostsWidget userId={_id}/>
                 </Box>
 
                 {isNonMobileScreen && (
